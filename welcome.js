b7d858532857e837357eaf9fef12e29e5e68dca9
Amira Anuar <amiraanuar@gmail.com>
2017-12-04 22:50:31 -0800

    Fix spacing for welcomejs

--- welcome.js
+++ welcome.js
@@ -1,22 +1,22 @@
 exports.handler = function(context, event, callback) {
-	let twiml = new Twilio.twiml.VoiceResponse();
-	let amiraPhoneNumber = context.AMIRA_PHONE_NUMBER;
+    let twiml = new Twilio.twiml.VoiceResponse();
+    let amiraPhoneNumber = context.AMIRA_PHONE_NUMBER;
     let callerName = event.SpeechResult || null;
     let notification = 'You have a guest';
 
     if (callerName) {
         twiml.say(callerName);
-      	notification = notification + ' named ' + callerName;
+        notification = notification + ' named ' + callerName;
     }
-	twiml.say('Welcome to Ameera and Teddy\'s place');
+    twiml.say('Welcome to Ameera and Teddy\'s place');
 
-  	// Should play the 9 key to let people in.
-  	// Maybe replace with digits and see if that works.
+    // Should play the 9 key to let people in.
+    // Maybe replace with digits and see if that works.
     twiml.play('/assets/9.mp3');
-  	twiml.play(null, {digits: 9});
+    twiml.play(null, {digits: 9});
 
-  	// Notify the relevant parties
- 	twiml.sms(notification, {to: '12035074786'});
+    // Notify the relevant parties
+    twiml.sms(notification, {to: '12035074786'});
 
-	callback(null, twiml);
+    callback(null, twiml);
 };
